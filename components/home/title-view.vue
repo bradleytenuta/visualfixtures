c7f4b978d6c767db50c821ebcfbf5a3f7dc647ee
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2020-10-08 11:20:21 +0100

    Added a country query param to homepage athletics button.

--- components/home/title-view.vue
+++ components/home/title-view.vue
@@ -25,7 +25,7 @@
         </p>
 
         <!-- Go Button -->
-        <v-btn nuxt tile large to="/athletics" class="primary">
+        <v-btn nuxt tile large to="/athletics?country=GBR" class="primary">
           Athletics Fixtures
           <v-icon right>mdi-arrow-right</v-icon>
         </v-btn>
