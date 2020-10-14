df80472c6c4d2eb9acec909f8bd8b445c63b6f32
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2020-10-14 11:18:54 +0100

    Fixed and added unit tests.

---
+++ test/unit/components/fixtures/map/map-view.spec.js
@@ -0,0 +1,41 @@
+// Libraries
+import Vuex from 'vuex'
+
+// Components
+import mapView from '~/components/fixtures/map/map-view'
+
+// Utilities
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { mockCompetitionTree, mockCompetition } from '~/test/mocks'
+
+// mapView test suite.
+describe('mapView', () => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+
+  // Creates a Mock Vuex Store.
+  const mockActions = {
+    changeActiveComp: jest.fn(),
+  }
+  const store = new Vuex.Store({
+    state: { activeComp: mockCompetition },
+    actions: mockActions,
+  })
+
+  // The wrapper Data to be used by all unit tests.
+  const wrapperData = {
+    localVue,
+    store,
+    stubs: {
+      NuxtLink: true,
+    },
+    propsData: {
+      viewableBranches: [...mockCompetitionTree],
+      scrollCounter: 1,
+    },
+  }
+
+  it('should render the mapView component', () => {
+    expect(shallowMount(mapView, wrapperData)).toBeTruthy()
+  })
+})
