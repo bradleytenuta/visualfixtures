a6478da43f75400d47385435ef952eb4b1acc9c4
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2020-10-24 10:17:37 +0100

    Removed snippet layout to reduce duplication. Removed lots of diplication in page. Added meta data to help support SEO support.

--- pages/snippets/athletics.vue
+++ pages/snippets/athletics.vue
@@ -1,21 +1,37 @@
 <template>
-  <calendar :countries="countries" sport="athletics" isSnippet />
+  <calendar :countries="countries" :sport="$options.sportName" isSnippet />
 </template>
 
 <script>
 import calendar from '~/components/fixtures/calendar.vue'
 import { sports } from '~/services/jsonLinks'
 
+// Reduces duplication of sport name.
+const sportName = 'athletics'
+
 export default {
-  name: 'athletics',
-  layout: 'snippets',
+  name: sportName,
+  layout: 'fixtures',
+  sportName,
+  head() {
+    return {
+      title: sportName.charAt(0).toUpperCase() + sportName.slice(1) + ' Snippet',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: `View ${sportName} fixtures near you. Designed for external viewing.`,
+        },
+      ],
+    }
+  },
   components: {
     calendar,
   },
   data() {
     return {
       countries: sports.find((sport) => {
-        return sport.name == 'athletics'
+        return sport.name == sportName
       }).countries,
     }
   },
